19d8445b6cebe97bc32c84c3d14e5d639a105188
kervias <cxz.kervia@gmail.com>
2021-03-16 19:45:37 +0800

    fix bug

--- src/spiders/template.py
+++ src/spiders/template.py
@@ -2,50 +2,17 @@
     html模板
 '''
 
-item = """
-<div class="page-header">
-<div class="alert alert-info" role="alert">{name}</div>
-             <a href="{url}"><h2>{title}<span class="badge">{tag}</span></h2></a>
-        </div>
-        <div class="jumbotron">
-            <p>
-                {content}    
-            </p>
-        </div>
+item = \
 """
-
-item_error = """
-    <div class="page-header">
-    <div class="alert alert-info" role="alert">{name}</div>
+    <div class="block">
+        <header><a href="{url}">{name}</a><span
+                id='label'>{tag}</span><span id="title">{title}</span>
+        </header>
+        <div class='content'>
+            {content}
         </div>
-        <div class="jumbotron">
-            <p>
-                {content}    
-            </p>
-        </div>
+    </div>
 """
-
-htmlT2 = """
-        <!-- 最新版本的 Bootstrap 核心 CSS 文件 -->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css"
-            integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-        <!-- 可选的 Bootstrap 主题文件（一般不用引入） -->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css"
-            integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-        <!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js"
-            integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
-        </script>
-        <div class="container">
-            <div class="row">
-            <div class="col-6">
-                {}
-            </div>
-            </div>
-        </div>
-        """
 
 HTMLT1 = \
 """
@@ -115,28 +82,20 @@ HTMLT1 = \
 </head>
 
 <body>
-
-    <div class="block">
-        <header><a href="{url}">{name}</a><span
-                id='label'>{tag}</span><span id="title">{title}</span>
-        </header>
-        <div class='content'>
-            {content}
-        </div>
-    </div>
+    {}
 </body>
 </html>
 """
 class TEMPLATE(object):
     def __init__(self, data):
         self.data = data
-        temp = HTMLT1
+        temp = ""
         for name, li in data.items():
             if li != None:
                 temp += item.format(name=name, url=li['url'],title=li['title'],tag=li['tag'],content=li['content'])
             else:
                 temp += item.format(name=name, content="获取信息失败")
-        self.ret = temp
+        self.ret = HTMLT1.format(temp)
         
     def template(self):
         return self.ret
